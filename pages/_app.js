382f2aa7ad5fd6690ac51e1c15d47270edad6b0a
Nataliia <koshmaknatalia@gmail.com>
2026-03-16 17:49:05 -0600

    add favicon

--- pages/_app.js
+++ pages/_app.js
@@ -42,6 +42,24 @@ export default function App({ Component, pageProps }) {
               name="viewport"
               content="width=device-width, initial-scale=1.0"
             />
+            <link
+              rel="apple-touch-icon"
+              sizes="180x180"
+              href="/apple-touch-icon.png"
+            ></link>
+            <link
+              rel="icon"
+              type="image/png"
+              sizes="32x32"
+              href="/favicon-32x32.png"
+            ></link>
+            <link
+              rel="icon"
+              type="image/png"
+              sizes="16x16"
+              href="/favicon-16x16.png"
+            ></link>
+            <link rel="manifest" href="/site.webmanifest"></link>
           </Head>
 
           <div className="layout">
